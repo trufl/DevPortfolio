943458c697889e3829a1d099881419ab73161e16
Tristan Saragosa <endlesstruffle80@gmail.com>
2023-04-17 08:34:06 -0700

    Add color pallets
    Adds some test color pallets

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -8,8 +8,17 @@ module.exports = {
       backgroundImage: (theme) => ({
         'royal-gradient': 'linear-gradient(to right, #4169E1, #8A2BE2)',
       }),
+      colors: {
+        'dark-black': '#393646',
+        'dark-charcoal': '#4F4557',
+        'dark-gray': '#6D5D6E',
+        'dark-white': '#F4EEE0',
+        'light-cream': '#FFFBCC',
+        'light-red': '#FD2E2E',
+        'light-cherry': '#CF1B1B',
+        'light-maroon': '#900D0D',
+      },
     },
   },
   plugins: [],
 }
-
